489d35bb9d6e431f61b8f44ae34a8b937b043f00
Djole <bozovic.djole@yahoo.com>
2019-07-13 19:08:17 +0200

    added less

---
+++ src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.less';
+function HomePage() {
+  return (
+    <div class="app-color">
+      <h1>moj te koka</h1>
+    </div>
+  );
+
+}
+
+export default HomePage;
